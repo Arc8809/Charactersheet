734bc9539e8e1fbd1b258bf53161e7a76da8f985
Arc8809 <arc8809@rit.edu>
2026-01-19 18:27:07 -0500

    Added reference pdfs and beginning basic structures

--- Character-Sheet/src/App.jsx
+++ Character-Sheet/src/App.jsx
@@ -1,33 +1,39 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const Strength = 0
+  const Dexterity = 0
+  const Constitution = 0
+  const Intelligence = 0
+  const Wisdom = 0
+  const Charisma = 0
+  const MaxHp = 0
+  const TempHp = 0
+  const Ac = 0
+  const Speed = 0
+  const Initiative = 0
+  const HitDice = '1d4'
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>Character Sheet</h1>
       <div className="card">
+       <div className="stats">
+        <h2>Strength: {Strength}</h2>
+        <h2>Dexterity: {Dexterity}</h2>
+        <h2>Constitution: {Constitution}</h2> 
+        <h2>Intelligence: {Intelligence}</h2>
+        <h2>Wisdom: {Wisdom}</h2>
+        <h2>Charisma: {Charisma}</h2>
+       </div>
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+       
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
---
+++ Character-Sheet/src/Top.jsx
@@ -0,0 +1,22 @@
+function Top() {
+const CharacterName = ""
+const CLvl = ""
+const TotalLvl= ""
+const Class = ""
+const Background = ""
+const Race= ""
+const Alignment= ""
+const XP= ""
+
+
+  return (
+    <div>
+        <h1>Character Name: {CharacterName}</h1>
+        <h2> </h2>
+    
+    
+    </div>
+  )
+
+}
+export default Top
